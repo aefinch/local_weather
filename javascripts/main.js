57dffcc49ee6b870b86b05e91012c4e324001956
Andrea Finch <aefinch@vt.edu>
2017-05-06 01:13:39 -0500

    printing current weather

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,32 +6,57 @@ $(document).ready(function(){
 		e.preventDefault();
 	});
 
-	const loadWeather = (zip) => {
+	
+	
+	const loadCurrentWeather = (zip) => {
 		return new Promise ((resolve, reject) => {
-			$.ajax(`http://api.openweathermap.org/data/2.5/forecast/daily?zip=${zip},us&appid=${apiKey}`)
+			$.ajax(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}&units=imperial`)
 			.done((data) => resolve(data))
 			.fail((error) => reject(error));
 		});
 	};
+		// const loadForecast = (zip) => {
+		// 	return new Promise ((resolve, reject) => {
+		// 		$.ajax(`http://api.openweathermap.org/data/2.5/forecast/daily?zip=${zip},us&appid=${apiKey}&units=imperial`)
+		// 		.done((data2) => resolve(data2))
+		// 		.fail((error2) => reject(error2));
+		// 	});
+		// };
+		
+	
 
-	const getZip = ()=>{
+	const printCurrentWeather = ()=>{
 		zipCode = $("#zipBox").val();
 		if (zipBox.validity.patternMismatch) {
 			
 		} else{
-			// console.log(zipCode);
-			loadWeather(zipCode).then((results)=>{
-				console.log(results);
+			
+			loadCurrentWeather(zipCode).then((results)=>{
+				$(".jumbotron").html("");
+				let currentTemp = results.main.temp;
+				let currentConditions = results.clouds.all;
+				let currentAirPressure = results.main.pressure;
+				let currentWindSpeed = results.wind.speed;
+				$(".jumbotron").append(`<h1>Current Weather</h1>`);
+				$(".jumbotron").append(`<h3>Temperature: ${currentTemp}&#176; F</h3>`);
+				$(".jumbotron").append(`<h4>Conditions: ${currentConditions}% Cloudy</h4>`);
+				$(".jumbotron").append(`<h4>Air Pressure: ${currentAirPressure}</h4>`);
+				$(".jumbotron").append(`<h4>Wind Speed: ${currentWindSpeed}mph`);
+				$(".jumbotron").append(`<button>3 Day Forecast</button>`);
+				$(".jumbotron").append(`<button>7 Day Forecast</button>`);
+
+			}).catch((error) => {
+				console.log("current weather error", error);
 			});
 		}
 	};
 
 	const whichKey = () => {
 		if(event.keyCode===13) {
-			getZip();
+			printCurrentWeather();
 		}
 	};
-	$("#submitZip").click(getZip);
+	$("#submitZip").click(printCurrentWeather);
 	$("#submitZip").keyup(whichKey);
 
 });
